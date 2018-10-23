53b9faefabd4717dcea86bf6d5141932584491ea
Luca Cozzuto <lucacozzuto@gmail.com>
2018-10-23 12:56:34 +0200

    parser rewritten

--- bin/parse.py
+++ bin/parse.py
@@ -33,6 +33,14 @@ def options_arg():
     else: parser.print_help()
     return (opts)
 
+def getFeatureStrand(feturetype, strand):
+	featurestrand = feturetype
+	if (feturetype == "arrow" and strand == "forward"):
+		featurestrand = "clockwise-arrow"
+	if (feturetype == "arrow" and strand == "reverse"):
+		featurestrand = "counterclockwise-arrow"
+	return featurestrand
+	
 opts = options_arg()
 fafile = opts.fafile
 binfile = opts.binfile
@@ -43,35 +51,24 @@ seqname = opts.seqname
 minidentity = 95
 maxtolerance = 5
 
-# trans code
-trans_code = {'HYB': 'score',
-'LOC': 'score',
-'ORI': 'origin_of_replication',
-'OTH': 'score',
-'PRO': 'promoter',
-'REG': 'regulatory_sequence',
-'REP': 'gene',
-'INS': 'gene',
-'SEL': 'gene',
-'TAG': 'score',
-'TER': 'terminator'}
-
-# gb trans code
-gb_trans_code = {'HYB': 'misc_feature',
-'LOC': 'misc_feature',
-'ORI': 'rep_origin',
-'OTH': 'misc_feature',
-'PRO': 'promoter',
-'REG': 'misc_feature',
-'INS': 'CDS',
-'REP': 'CDS',
-'SEL': 'CDS',
-'TAG': 'misc_feature',
-'TER': 'terminator'}
+# feat info code
+feat_info = defaultdict(dict)
+feat_info['HYB'] = {'color' : 'red', 'style' : "arc", "label": "Hyper activation domain", "gb": "misc_feature"} # Hyper activation domain 
+feat_info['LOC'] = {'color' : 'gray', 'style' : "arc", "label": "Locus", "gb": "misc_feature"} # Locus
+feat_info['ORI'] = {'color' : 'black', 'style' : "arc", "label": "Origin of replication", "gb": "rep_origin"} # Origin of replication
+feat_info['OTH'] = {'color' : 'gray', 'style' : "arc", "label": "Other", "gb": "misc_feature"}
+feat_info['PRO'] = {'color' : 'green', 'style' : "arrow", "label": "Promoter", "gb": "promoter"} # Promoter
+feat_info['REG'] = {'color' : 'lime', 'style' : "arc", "label": "Regulatory sequence", "gb": "misc_feature"}
+feat_info['REP'] = {'color' : 'yellow', 'style' : "arrow", "label": "Reporter gene", "gb": "Marker"} 
+feat_info['INS'] = {'color' : 'red', 'style' : "arrow", "label": "Inserted gene", "gb": "CDS"} # Insert 
+feat_info['SEL'] = {'color' : 'orange', 'style' : "arrow", "label": "Gene for selection", "gb": "CDS"} # gene for selection TAG
+feat_info['TAG'] = {'color' : 'gray', 'style' : "arrow", "label": "affinity TAG", "gb": "misc_feature"} # Affinity TAG
+feat_info['TER'] = {'color' : 'maroon', 'style' : "arc", "label": "Terminator", "gb": "terminator"} # Terminator 
+feat_info['RESTR'] = {'color' : 'blue', 'style' : "arc", "label": "Unique restriction site", "gb": "misc_binding"} # Restriction site
 
 # read input and prepare output
 fahandle	= open(fafile, 'rb')
-outhandle	= open(outfile + ".tab", 'w+')
+xmlhandle	= open(outfile + ".xml", 'w+')
 outhandlegb	= open(outfile + ".gbk", 'w+')
 outhandlog	= open(outfile + ".log", 'w+')
 inhandle	= open(binfile, 'rb')
@@ -104,11 +101,16 @@ if (len(gbarr)>0):
 
 gbseq = gbseq + "//"
 	
-outstring = "#" + seqname + "\n%" + str(seqsize) + "\n" + "!strand	slot	start	stop	opacity	thickness	type	label\n"
+outXMLstring = '<?xml version="1.0" encoding="ISO-8859-1"?>' + "\n"
+outXMLstring += '<cgview backboneRadius="160" sequenceLength="' + str(seqsize) + '">' + "\n"
+
 gbkstring = "LOCUS\t" + seqname + "\t" + str(seqsize) + " bp\tDNA\tcircular\nFEATURES\tLocation/Qualifiers\n"
 
 coords = defaultdict(dict)
 inserts = []
+xmlarr = defaultdict(dict)
+xmlarrOrig = defaultdict(dict)
+
 # read blast tabular output file
 with inhandle as fi:
 	for line in fi:
@@ -127,70 +129,71 @@ with inhandle as fi:
 
 		if (sstart-send<0):	#plus strand
 			strand = "forward"
-			gbcoords =  str(qstart) + ".." + str(qend)
 		else:
 			strand = "reverse"
-			gbcoords =  "complement(" + str(qstart) + ".." + str(qend) + ")"
 
-		rowsrting   = strand + "\t1\t" + str(qstart) + "\t" + str(qend) + "\t1\t1\t" + trans_code[feattype] + "\t" + featname + "-" + feattype + "\n"
-		rowgbstring = "\t" +  gb_trans_code[feattype] + "\t" + gbcoords + "\n" + "\t\t/label=" + featname + "\n"			
-					
+		feat = {"featname": featname, "featsize": featsize, "feattype": feattype, "start":qstart, "end":qend, "strand":strand}
+			
 		if (featsize<=50):
 			minsize = int(round(featsize/10))
 		else:
 			minsize = maxtolerance
 		
 		if (featsize - qlength <= minsize and identity>=minidentity): 
-			outstring = outstring + rowsrting
-			gbkstring = outstring + rowgbstring
-			if (feattype == "INS"): 
-				inserts.append(featname)
+			if feattype not in xmlarr:
+				xmlarr[feattype] = []
+				xmlarr[feattype].append( feat )
+			else:
+				xmlarr[feattype].append( feat )
 				
 		if (qstart <= maxtolerance and identity>=minidentity): # special case circular genome!!
-			if (featname not in coords): # inizialize
-				coords[featname]["feattype"]     	= feattype
-				coords[featname]["sizeBefore0"]     = 0
-				coords[featname]["sizeAfter0"]      = qend
-				coords[featname]["stringAfter0"]    = rowsrting
-				coords[featname]["stringBefore0"]   = ""
-				coords[featname]["stringGBAfter0"]  = rowgbstring
-				coords[featname]["stringGBBefore0"] = ""
-			# Override in case the next match is longer than the previous and the subject matched is the same
-			if (coords[featname]["sizeAfter0"]< qend):
-				coords[featname]["sizeAfter0"]     = qend
-				coords[featname]["stringAfter0"]   = rowsrting
-				coords[featname]["stringGBAfter0"] = rowgbstring
-			
+			if ("A") not in xmlarrOrig[featname].keys():
+				xmlarrOrig[featname]["A"] = feat
+			elif(xmlarrOrig[featname]["A"]['end'] - xmlarrOrig[featname]["A"]['start'] < feat['end'] - feat['start']):
+				xmlarrOrig[featname]["A"] = feat
+										
 		if (qend >= seqsize - maxtolerance and identity>=minidentity):
-			if (featname not in coords): # inizialize
-				coords[featname]["feattype"]     	= feattype
-				coords[featname]["sizeBefore0"]    = seqsize - qstart
-				coords[featname]["sizeAfter0"]     = 0
-				coords[featname]["stringAfter0"]   = ""
-				coords[featname]["stringBefore0"]  = rowsrting
-				coords[featname]["stringGBAfter0"] = rowgbstring
-				coords[featname]["stringGBBefore0"] = ""
-			# Override in case the next match is longer than the previous and the subject matched is the same	
-			if (coords[featname]["sizeBefore0"] < seqsize - qstart):
-				coords[featname]["sizeBefore0"]    = seqsize - qstart
-				coords[featname]["stringBefore0"]  = rowsrting
-				coords[featname]["stringGBBefore0"] = rowgbstring
+  			# Override in case the next match is longer than the previous and the subject matched is the same       
+			if ("B") not in xmlarrOrig[featname].keys():
+				xmlarrOrig[featname]["B"] = feat
+			elif(xmlarrOrig[featname]["B"]['end'] - xmlarrOrig[featname]["B"]['start'] < feat['end'] - feat['start']):
+				xmlarrOrig[featname]["B"] = feat
 
-for featname_break in coords:
-	size = coords[featname_break]["sizeBefore0"] + coords[featname_break]["sizeAfter0"]
-	if (size<=50):
+
+for featname_broken in xmlarrOrig:
+	sizeA = 0
+	sizeB = 0
+	start = 0
+	end = seqsize
+	if ("A") in xmlarrOrig[featname_broken].keys():
+		featsize = xmlarrOrig[featname_broken]["A"]["featsize"]
+		feattype = xmlarrOrig[featname_broken]["A"]["feattype"]
+		sizeA = xmlarrOrig[featname_broken]["A"]['end'] - xmlarrOrig[featname_broken]["A"]['start']
+
+		# Start is bigger than end when crossing the origin
+		start = xmlarrOrig[featname_broken]["A"]['start']
+		end = xmlarrOrig[featname_broken]["A"]['end']
+		
+	if ("B") in xmlarrOrig[featname_broken].keys():
+		featsize = xmlarrOrig[featname_broken]["B"]["featsize"]
+		feattype = xmlarrOrig[featname_broken]["B"]["feattype"]
+		sizeB = xmlarrOrig[featname_broken]["B"]['end'] - xmlarrOrig[featname_broken]["B"]['start']
+		
+		start = max(start, xmlarrOrig[featname_broken]["B"]['start'])
+		end = min(end, xmlarrOrig[featname_broken]["B"]['end'])
+	size = sizeA+sizeB
+	feat = {"featname": featname_broken, "feattype": feattype, "start":start, "end":end, "strand":strand}
+	if (featsize<=50):
 		minsize = int(round(size/10))
 	else:
 		minsize = maxtolerance
-
-	if (size>=minsize):	
-		outstring = outstring  + coords[featname_break]["stringBefore0"] + coords[featname_break]["stringAfter0"]
-		gbkstring = gbkstring  + coords[featname_break]["stringGBBefore0"] + coords[featname_break]["stringGBAfter0"]
-		if (coords[featname_break]["feattype"] == "INS"):
-			inserts.append(featname_break)
-
-gbkstring = gbkstring + "ORIGIN\n" + gbseq + "\n"
-
+	if (featsize - size <= minsize): 
+		if feattype not in xmlarr:
+			xmlarr[feattype] = []
+			xmlarr[feattype].append( feat )
+		else:
+			xmlarr[feattype].append( feat )
+		
 #parsing Restrict file from EMBOSS. Extracts the coordinates
 with rihandle as fi:
 	for line in fi:
@@ -206,10 +209,50 @@ with rihandle as fi:
 				end = int(fields[2]) 
 				if (end>seqsize):
 					end = end - seqsize
-				outstring = outstring + strand + "\t2\t" + str(start) + "\t" + str(end) + "\t1\t1\tunique_restriction_site\t" + fields[4] + "\n"
+				feat = 	{"featname": fields[4], "feattype": "RESTR", "start":str(start), "end": str(end), "strand": strand}
+				if "RESTR" not in xmlarr:
+					xmlarr["RESTR"] = []
+					xmlarr["RESTR"].append( feat )
+				else:
+					xmlarr["RESTR"].append( feat )
 
-outhandle.write(outstring)
-outhandle.close()
+#Write XML files and GeneBank file
+for feature_type, features in xmlarr.iteritems():
+	featcolor = feat_info[feature_type]['color']
+	outXMLstring += " <featureSlot strand=\"direct\">" + "\n"
+	for featdata in features:
+		start = featdata['start']
+		end = featdata['end']
+		featname = featdata['featname']
+		featstrand = getFeatureStrand(feat_info[feature_type]['style'], featdata['strand'])
+		outXMLstring += "  <feature color=\""+ featcolor +"\" decoration=\"" + featstrand + "\" label=\"" + featname + "\">" + "\n"
+		outXMLstring += "    <featureRange start=\"" + str(start) + "\" stop=\"" + str(end) + "\" />" + "\n"
+		outXMLstring += "    </feature>" + "\n"
+
+		if (featdata['strand'] == "reverse"):
+			gbcoords = "complement(" + str(start) + ".." + str(end) + ")"
+		else:
+			gbcoords = str(start) + ".." + str(end)
+		gbkstring += feat_info[feature_type]['gb'] + "\t" + gbcoords + "\n" + "\t\t/label=" + featname + "\n"
+		
+	outXMLstring += " </featureSlot>" + "\n"
+outXMLstring += "<legend position=\"upper-right\">" + "\n"
+
+for feature_type, features in xmlarr.iteritems():
+	featcolor = feat_info[feature_type]['color']
+	featlab = feat_info[feature_type]['label']
+	outXMLstring += "  <legendItem text=\"" + featlab + "\" drawSwatch=\"true\" swatchColor=\"" + featcolor + "\" />" + "\n"
+	
+outXMLstring += "</legend>" + "\n"
+outXMLstring += '</cgview>' + "\n"
+
+
+gbkstring += "ORIGIN\n" + gbseq + "\n"
+
+
+
+xmlhandle.write(outXMLstring)
+xmlhandle.close()
 
 outhandlegb.write(gbkstring)
 outhandlegb.close()
@@ -218,3 +261,5 @@ if len(inserts)==0:
 	 inserts.append("-")
 outhandlog.write(",".join(inserts))
 outhandlog.close()
+
+
