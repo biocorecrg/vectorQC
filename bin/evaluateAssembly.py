fbe1aa7af7179d32b17abb4b906b08d4a4a5bc8b
Luca Cozzuto <lucacozzuto@gmail.com>
2019-04-08 17:59:26 +0200

    implementing filtering of low-covered contigs

--- bin/evaluateAssembly.py
+++ bin/evaluateAssembly.py
@@ -45,32 +45,52 @@ seqout   = ""
 fastaout = ""
 header   = ">" + seqname
 log 	 = ""
-# read fasta file for getting the size
+fasta_seqs = {}
+totlen = 0
+weightcov = 0 
+
+# read fasta file for filtering low covered reads
 with fahandle as fa:
 	for line in fa:
 		if (line[:1]== ">"): # header
-			num += 1
-		else:
-			seqout += line.rstrip() 
+			header = line[1:].rstrip()
+			vals = header.split("_")
+			length = int(vals[3])
+			cov = float(vals[5])
+			totlen += length
+			weightcov += length*cov
 
-if (num > 1):
+meancov = weightcov/totlen
+fahandle	= open(fafile, 'rb')
+# read fasta file for filtering low covered reads
+with fahandle as fa:
+	for line in fa:
+		if (line[:1]== ">"): # header
+			header = line[1:].rstrip()
+			vals = header.split("_")
+			cov = float(vals[5])
+			if (cov>=(meancov*0.5) and cov<=(meancov*1.5)): 
+				fasta_seqs[header] = ""
+		elif (header in fasta_seqs) :
+			fasta_seqs[header] += line.rstrip() 
+
+num = len(fasta_seqs)
+if (num > 1): 
 	print "WARNING FOUND " + str(num) + " SCAFFOLDS in " + seqname + "\n"
+	for seqnames in fasta_seqs:
+		seqout += fasta_seqs[seqnames]
 else:
-	seqout = seqout[:-127]
+	for seqnames in fasta_seqs:
+		seqout += fasta_seqs[seqnames]	
+		seqout = seqout[:-(int(stretch))]
 	
 seqsize = str(len(seqout))
-seqout = header + "," + seqsize + "\n" + textwrap.fill(seqout, width=60)
+seqout = ">" + seqname + "," + seqsize + "\n" + textwrap.fill(seqout, width=60)
 	
 outhandle.write(seqout)			
-outhandle.close()		
+outhandle.close()
 
 outlhandle.write(seqname + "\t" + str(num) + "\t" + seqsize + "\n")	
-outlhandle.close()		
-
-
-
-
-
-	
+outlhandle.close()	
 
 
